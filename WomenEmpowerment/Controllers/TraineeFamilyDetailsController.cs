d9d762bfb07bd85b32096478e9b1980bbc55906e
ib4717 <ib4717@srmist.edu.in>
2022-07-29 18:38:16 +0530

    updated

---
+++ WomenEmpowerment/Controllers/TraineeFamilyDetailsController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WomenEmpowerment.Models;
+
+namespace WomenEmpowerment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TraineeFamilyDetailsController : ControllerBase
+    {
+        WomenEmpowermentContext db = new WomenEmpowermentContext();
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult PostTraineeFamilyDetails(TraineeFamilyDetail familyDetails)
+        {
+            try
+            {
+                db.TraineeFamilyDetails.Add(familyDetails);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok(familyDetails);
+        }
+
+        [HttpGet]
+        [Route("Get/{traineeId}")]
+        public IActionResult GetTraineeFamilyDetails(int traineeId)
+        {
+            var familyDetails = new TraineeFamilyDetail();
+            try
+            {
+                var data = db.TraineeFamilyDetails.ToList();
+                familyDetails = (from d in data where d.TraineeId == traineeId select d).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok(familyDetails);
+        }
+    }
+}
--- WomenEmpowerment/Startup.cs
+++ WomenEmpowerment/Startup.cs
@@ -47,7 +47,7 @@ namespace WomenEmpowerment
 
             app.UseCors(options =>
             {
-                options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                options.WithOrigins("http://localhost:4200/").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
             });
 
             app.UseSession();
